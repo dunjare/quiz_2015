233a52777bf2c6b502b9f87cebe352282a6332b8
dunjare <durepu.job@gmail.com>
2015-07-11 14:30:05 +0200

    3.- Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// paso 1b Importar controlador
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz_2015' });
 });
+
+// paso 1c Introducir nuevas rutas en el enrutador
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
