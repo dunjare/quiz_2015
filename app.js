991dd27e2d67245ddf81ed472ead68880d339dc6
dunjare <durepu.job@gmail.com>
2015-07-11 16:15:43 +0200

    4.- Agregar Marco a la aplicación con express-partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials'); // Agregar Marco de la aplicacion
 
 var routes = require('./routes/index'); // Importar enrutadores
 
@@ -13,6 +14,7 @@ var app = express();  // Crear aplicacion
 // Instalar generador de vistas EJS
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'ejs');
+app.use(partials()); // Layout
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
