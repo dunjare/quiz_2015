fb350efb134f2f4ee8d48adeca532134aa5a1b37
dunjare <durepu.job@gmail.com>
2015-07-12 17:59:09 +0200

    Mod 6  branch Creditos, agregar Autor

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+// modulo 6
+router.get('/author', function(req, res){
+	res.render('author');
+});
+
 module.exports = router;
