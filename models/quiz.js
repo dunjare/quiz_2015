11735c22fff24db601d71b984aff1f05a83b6719
dunjare <durepu.job@gmail.com>
2015-07-22 00:24:20 +0200

    Modulo 7.1: DB con sequelize.js y SQLite

---
+++ models/quiz.js
@@ -0,0 +1,9 @@
+// Modulo 7 Tabla Quiz
+// Definicion del modelo de Quiz con validación
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Quiz',
+    { pregunta: DataTypes.STRING,
+      respuesta: DataTypes.STRING
+    });
+}
