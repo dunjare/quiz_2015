506fd69c4e7fb3326d7f322cc5d27944df1415b5
dunjare <durepu.job@gmail.com>
2015-08-21 00:53:24 +0200

    Modulo 9 - Update P2P ejercicio

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -71,7 +71,19 @@ exports.create = function(req, res) {
   });
 };
 
+// Mod 9 Quiz 18 - Moderacion de Comentarios
+// GET /quizes/:quizId/comments/:commentId/publish
+
 exports.publish = function(req, res) {
+  req.comment.publicado = true;
+
+  req.comment.save( {fields: ["publicado"]})
+    .then( function(){ res.redirect('/quizes/'+ req.params.quizId);} )
+    .catch(function(error){next(error)});
+
+};
+
+/*exports.publish = function(req, res) {
   req.comment.publicado = true;
   req.comment.save({
     fields: ['publicado']
@@ -80,4 +92,4 @@ exports.publish = function(req, res) {
   }).catch(function(error) {
     next(error);
   });
-};
+};*/
